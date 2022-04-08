e5aa42a0fe78ace0422f327394ef0e86832d8c64
Denis Balandin <doublestar2013@yandex.ru>
2022-04-08 10:55:39 +0300

    Lesson 1 FinCode

--- NotesAPP/ViewController.swift
+++ NotesAPP/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     private let textView = UITextView()
     private let defaults = UserDefaults.standard
-    private let firstTitle = UITextField()
+    private let titleTextField = UITextField()
     private let myButton = UIBarButtonItem()
-    private let titleField = UITextField()
 
     private let mainTextView: UITextView = {
         let mainTextView = UITextView()
@@ -25,7 +24,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         mainTextView.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 14)
-        firstTitle.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 22)
+        titleTextField.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 22)
 
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(
@@ -36,43 +35,41 @@ class ViewController: UIViewController {
         )
         let myButton = UIBarButtonItem(title: "Готово", style: .plain, target: self, action: #selector(stopFocusing))
 
-        firstTitle.placeholder = "Название заметки"
+        titleTextField.placeholder = "Название заметки"
 
         mainTextView.text = defaults.string(forKey: "mainText")
-        firstTitle.text = defaults.string(forKey: "topText")
+        titleTextField.text = defaults.string(forKey: "topText")
 
         view.addSubview(mainTextView)
-        view.addSubview(titleField)
         view.addSubview(textView)
-        view.addSubview(firstTitle)
+        view.addSubview(titleTextField)
 
         view.backgroundColor = .white
         navigationItem.rightBarButtonItem = myButton
         setConstraints()
-        titleField.translatesAutoresizingMaskIntoConstraints = false
-        firstTitle.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
         mainTextView.becomeFirstResponder()
     }
 
     private func setConstraints() {
-        firstTitle.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        firstTitle.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        firstTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        titleTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        titleTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
 
         mainTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         mainTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         mainTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        mainTextView.topAnchor.constraint(equalTo: firstTitle.bottomAnchor).isActive = true
+        mainTextView.topAnchor.constraint(equalTo: titleTextField.bottomAnchor).isActive = true
     }
 
     @objc func stopFocusing() {
         mainTextView.resignFirstResponder()
-        firstTitle.resignFirstResponder()
+        titleTextField.resignFirstResponder()
     }
 
     @objc func save() {
         let mainText = mainTextView.text
-        let topView = firstTitle.text
+        let topView = titleTextField.text
         defaults.set(topView, forKey: "topText")
         defaults.set(mainText, forKey: "mainText")
     }
